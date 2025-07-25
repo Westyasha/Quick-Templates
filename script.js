aa33e7fba5c8685becff7ca649a81fd4db22ce72
Westyasha <vladflaik@gmail.com>
2025-07-25 20:55:40 +0300

    1.1
    background / patterns

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('image-modal');
     const modalImg = modal.querySelector('.modal-content');
     const closeModalButton = modal.querySelector('.modal-close-button');
+    const bubblesContainer = document.querySelector('.domains-bubbles');
 
     if (modal && modalImg && closeModalButton && galleryItems.length > 0) {
         const openModal = (src) => {
@@ -36,5 +37,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 closeModal();
             }
         });
+    }
+
+    if (bubblesContainer) {
+        for (let i = 0; i < 50; i++) {
+            const bubble = document.createElement('span');
+            const size = (Math.random() * 25) + 10;
+            bubble.style.width = `${size}px`;
+            bubble.style.height = `${size}px`;
+            bubble.style.left = `${Math.random() * 100}vw`;
+            bubble.style.animationDuration = `${(Math.random() * 10) + 15}s`;
+            bubble.style.animationDelay = `${Math.random() * 5}s`;
+            bubble.className = Math.random() > 0.5 ? 'blue' : 'pink';
+            bubblesContainer.appendChild(bubble);
+        }
     }
 });
